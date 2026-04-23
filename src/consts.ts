ab21f79caf93174523d4e874bb9839d0610ab55b
The Artisan's Grain <tcsstowell@gmail.com>
2026-04-23 09:18:41 -0500

    Update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "The Artisan's Grain";
-export const SITE_DESCRIPTION = "Wooden Bow ties crafted with care and precision. Each piece is a unique work of art, blending traditional craftsmanship with modern design. Discover our collection of elegant and stylish wooden bow ties, perfect for any occasion.";
+export const SITE_TITLE = "The Artisan's Grain Wooden Bow Ties";
+export const SITE_DESCRIPTION = "Epoxy Poured Wooden Bow ties crafted with care and precision. Each piece is a unique work of art, blending traditional craftsmanship with modern design. Discover our collection of elegant and stylish wooden bow ties, perfect for any occasion.";
